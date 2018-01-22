01413167d73e60e2ae0480daf445ac73d8575de6
php 23 <php.23@zecolmeia.cec.ime.usp.br>
2018-01-22 20:57:59 -0200

    testando cartao de credito com data do carbon

---
+++ exer.4.php
@@ -0,0 +1,21 @@
+<?php
+ class Cliente
+ {
+   public $nome; //atributo
+   public $codigo; // atributo
+
+ }
+ $x = new Cliente(); //criando objeto
+ $x ->nome = "Rafael Cosentino"; // atribuindo valor
+ $x ->codigo = "Codigo 1"; // atribuindo valor
+
+ echo "Nome: $x->nome" . PHP.EOL;
+ echo "codigo: $x->codigo" . PHP.EOL;
+
+ $y = new Cliente();
+ $y ->nome = "Jonas Hirata";
+ $y ->codigo = "Codigo 2";
+
+ echo "Nome: $x->nome" . PHP.EOL;
+ echo "codigo: $x->codigo" . PHP.EOL;
+?>
